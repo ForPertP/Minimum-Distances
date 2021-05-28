a09692e7d0f428fdbe1f3cdd4209a0b1be5f2200
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-28 19:15:17 +0900

    Update Minimum-Distances.cpp

--- Minimum-Distances.cpp
+++ Minimum-Distances.cpp
@@ -1,1 +1,28 @@
+int minimumDistances(vector<int> a)
+{
+	int min = a.size();
+	std::map<int, int> mp;
 
+	for (int i = 0; i < a.size(); ++i)
+	{
+		if (mp[a[i]])
+		{
+			int distance = i - mp[a[i]];
+			if (distance < min)
+			{
+				min = distance;
+			}
+		}
+		else
+		{
+			mp[a[i]] = i;
+		}
+	}
+
+	if (min == a.size())
+	{
+		return -1;
+	}
+
+	return min;
+}
