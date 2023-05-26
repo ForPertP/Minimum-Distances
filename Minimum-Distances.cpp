4475bb1460fb6b7644899b86fb29667a8fdbf15c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-26 13:53:30 +0900

    Update Minimum-Distances.cpp

--- Minimum-Distances.cpp
+++ Minimum-Distances.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
 int minimumDistances(vector<int> a)
 {
     int min = a.size();
