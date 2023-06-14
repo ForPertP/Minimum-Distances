9a84623fcb3fe18634a9759e52e416f4494e9d82
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-06-14 11:35:47 +0900

    Update Minimum-Distances.java

--- Minimum-Distances.java
+++ Minimum-Distances.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.math.*;
 import java.security.*;
 import java.text.*;
@@ -9,3 +10,58 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+    // Write your code here
+        int min = a.size();
+        int size = a.size();
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int i = 0; i < size; i++) {
+            if (mp.containsKey(a.get(i))) {
+                int distance = i - mp.get(a.get(i));
+                if (distance < min) {
+                    min = distance;
+                }
+                if (min == 1) {
+                    break;
+                }
+            } else {
+                mp.put(a.get(i), i);
+            }
+        }
+        
+        return (min == a.size()) ? -1 : min;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
